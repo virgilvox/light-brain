299b6a9c41d43e261245bf62feda9adc31db5b4d
virgilvox <mzara1@asu.edu>
2018-09-24 03:00:24 -0400

    added console logs

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ var Muse = nodeMuse.connect().Muse;
 
 Muse.on('connected', function(){
     // There's definitely a muse connected right now!
+    console.log("CONNECTED TO THE BRAIN MACHINE")
 });
 
 Muse.on('uncertain', function(){
     // For some reason, i'm not detecting the muse anymore at the moment...
     // Waiting for new signals to arrive...
+    console.log("stuff went bad, dunno what")
 })
 
 Muse.on('disconnected', function(){
     // Nope, it's definitely not connected anymore...
+    console.log("Disconnected!");
+});
+
+Muse.on('/muse/eeg', function(data){
+    // Receiving EEG signals!
+    console.log("Data: ", data);
 });
