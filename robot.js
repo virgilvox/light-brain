4e3c4528d55e051f85f44f16db7d36b89dd61ffa
virgilvox <mzara1@asu.edu>
2018-09-24 04:32:50 -0400

    but what if osc robot

---
+++ robot.js
@@ -0,0 +1,30 @@
+var five = require("johnny-five");
+var board = new five.Board();
+var osc = require("osc");
+
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 5000,
+    metadata: true
+});
+
+board.on("ready", function() {
+
+  // Create a standard `led` component instance
+  var led = new five.Led(4);
+
+  udpPort.open();
+
+  udpPort.on("ready", function () {
+    udpPort.on("message", function (oscMsg) {
+
+      var address = oscMsg.address;
+      if(address.includes("/muse/elements/beta_absolute")){
+        var value = oscMsg.args[0].value;
+        console.log(value);
+        if(value > 0.29){ console.log("on"); led.on();}
+        if(value < 0.29){ console.log("off"); led.off();}
+      }
+    });
+  });
+});
