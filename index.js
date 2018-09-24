bb962a1adab61bf805b87580ef8b271193e2a547
virgilvox <mzara1@asu.edu>
2018-09-24 03:37:37 -0400

    but what if

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-var nodeMuse = require("node-muse");
+const { MUSE_SERVICE, MuseClient, zipSamples, channelNames } = require('muse-js');
+const noble = require('noble');
+const bluetooth = require('bleat').webbluetooth;
 
-var Muse = nodeMuse.connect().Muse;
+async function connect() {
+    let device = await bluetooth.requestDevice({
+        filters: [{ services: [MUSE_SERVICE] }]
+    });
+    const gatt = await device.gatt.connect();
+    const client = new MuseClient();
+    await client.connect(gatt);
+    await client.start();
+    // Now do whatever with muse client...
+    client.eegReadings.subscribe(reading => {
+    console.log(reading);
+    });
+    client.telemetryData.subscribe(telemetry => {
+      console.log(telemetry);
+    });
+    client.accelerometerData.subscribe(acceleration => {
+      console.log(acceleration);
+    });
+}
 
-Muse.on('connected', function(){
-    // There's definitely a muse connected right now!
-    console.log("CONNECTED TO THE BRAIN MACHINE")
-});
-
-Muse.on('uncertain', function(){
-    // For some reason, i'm not detecting the muse anymore at the moment...
-    // Waiting for new signals to arrive...
-    console.log("stuff went bad, dunno what")
-})
-
-Muse.on('disconnected', function(){
-    // Nope, it's definitely not connected anymore...
-    console.log("Disconnected!");
-});
-
-Muse.on('/muse/eeg', function(data){
-    // Receiving EEG signals!
-    console.log("Data: ", data);
+noble.on('stateChange', (state) => {
+    if (state === 'poweredOn') {
+        connect();
+    }
 });
