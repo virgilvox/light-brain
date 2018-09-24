e280613b91ffaeeffa4ea681378928436b6690b8
virgilvox <mzara1@asu.edu>
2018-09-24 04:02:18 -0400

    but what if osc

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-const { MUSE_SERVICE, MuseClient, zipSamples, channelNames } = require('muse-js');
-const noble = require('noble');
-const bluetooth = require('bleat').webbluetooth;
+var osc = require("osc");
 
-async function connect() {
-    let device = await bluetooth.requestDevice({
-        filters: [{ services: [MUSE_SERVICE] }]
-    });
-    const gatt = await device.gatt.connect();
-    const client = new MuseClient();
-    await client.connect(gatt);
-    await client.start();
-    // Now do whatever with muse client...
-    client.eegReadings.subscribe(reading => {
-    console.log(reading);
-    });
-    client.telemetryData.subscribe(telemetry => {
-      console.log(telemetry);
-    });
-    client.accelerometerData.subscribe(acceleration => {
-      console.log(acceleration);
-    });
-}
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 5000,
+    metadata: true
+});
 
-noble.on('stateChange', (state) => {
-    if (state === 'poweredOn') {
-        connect();
+udpPort.open();
+
+udpPort.on("ready", function () {
+  udpPort.on("message", function (oscMsg) {
+
+    var address = oscMsg.address;
+    if(address.includes("/muse/elements")){
+      console.log("Data: ", oscMsg);
     }
+  });
 });
